b6cdf3b2aa2b1699cb13f771b294b75f26fc983f
{{myskiva}} <{{arseniymyskiv@gmail.com}}>
2022-09-24 11:22:10 +0300

    task 1

--- lab_2.cs
+++ lab_2.cs
@@ -1,29 +1,102 @@
-class Adress
+using Microsoft.VisualBasic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace task1
 {
-    public int index = "Undefined";
-    public string country = "Undefined";
-    public string city = "Undefined";
-    public string street = "Undefined";
-    public int house = "Undefined";
-    public int apartment = "Undefined";
-
-    public void Print()
+    class Adress
     {
-        Console.WriteLine($"Index: {index} Country: {country} City: {city} Street: {street} House: {house} Apartment: {apartment}");
-    }
-}
+        private int index;
+        private string country;
+        private string city;
+        private string street;
+        private int house;
+        private int apartment;
 
-my Adress = new Adress();
-int indexAdress = my.index;
-string countryAdress = my.country;
-string cityAdress = my.city;
-string streetAdress = my.street;
-int houseAdress = my.house;
-int apartmentAdress = my.apartment;
-Console.WriteLine($"Index: {indeAdressx} Country: {countryAdress} City: {cityAdress} Street: {streetAdress} House: {houseAdress} Apartment: {apartmentAdress}");
+        public int Index
+        {
+            get 
+            { 
+                return index; 
+            }
+            set
+            {
+                index = value;
+            }
+        }
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string Street
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                street = value;
+            }
+        }
+        public int House
+        {
+            get
+            {
+                return house;
+            }
+            set
+            {
+                house = value;
+            }
+        }
+        public int Apartment
+        {
+            get
+            {
+                return apartment;
+            }
+            set
+            {
+                apartment = value;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Index: {index} Country: {country} City: {city} Street: {street} House: {house} Apartment: {apartment}");
+        }
+    }
+
+    my Adress = new Adress();
+    int indexAdress = my.index;
+    string countryAdress = my.country;
+    string cityAdress = my.city;
+    string streetAdress = my.street;
+    int houseAdress = my.house;
+    int apartmentAdress = my.apartment;
+    Console.WriteLine($"Index: {indexAdress} Country: {countryAdress} City: {cityAdress} Street: {streetAdress} House: {houseAdress} Apartment: {apartmentAdress}");
 my.index = 55555;
 my.country = "Ukraine";
 my.city = "Kyiv";
 my.street = "Khreshchatyk";
 my.house = 25;
 my.apartment = 50;
+}
